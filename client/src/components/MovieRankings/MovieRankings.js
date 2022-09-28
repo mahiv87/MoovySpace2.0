810cb4cde756cee8c916a452740a82c839c4c740
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-09-27 22:19:54 -0600

    cleaned up file

--- client/src/components/MovieRankings/MovieRankings.js
+++ client/src/components/MovieRankings/MovieRankings.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './MovieRankings.css';
-import { AppContext } from '../../App';
-import { useMutation, useQuery } from '@apollo/client';
-import { useParams } from 'react-router-dom';
 // Update to grab actual mutations for movie rankings
 import { ADD_USER, FAVORITE_MOVIE } from '../../utils/mutations';
 import rater from 'rater-js';
