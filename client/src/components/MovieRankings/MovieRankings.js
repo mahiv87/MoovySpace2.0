3613aa6c35c2cca3f727bb9b8b6a24c3d30317d8
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-09-27 22:01:04 -0600

    imported rater

--- client/src/components/MovieRankings/MovieRankings.js
+++ client/src/components/MovieRankings/MovieRankings.js
@@ -5,7 +5,7 @@ import { useMutation, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 // Update to grab actual mutations for movie rankings
 import { ADD_USER, FAVORITE_MOVIE } from '../../utils/mutations';
-import Ratings from 'react-ratings-declarative';
+import rater from 'rater-js';
 
 const MovieRankings = () => {};
 
